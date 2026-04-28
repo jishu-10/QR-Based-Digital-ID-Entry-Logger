67fc29d49d7f407a112cc785844a4e76d53b48cd
Snehanshu Das <156137321+jishu-10@users.noreply.github.com>
2026-04-29 00:44:27 +0530

    Update appsScriptWebAppUrl in runtime-config.js

--- assets/js/runtime-config.js
+++ assets/js/runtime-config.js
@@ -1,6 +1,6 @@
 window.APP_RUNTIME_CONFIG = Object.freeze({
   appsScriptWebAppUrl:
-    'https://script.google.com/macros/s/AKfycbylNzZAgZiuGRCRt7bQjlLosal-DrEkc31AN4ZngDx19g6W5WZimCHf1m36c9UcQ2gULg/exec',
+    'https://script.google.com/macros/s/AKfycby9B1BgbkLdnQ_g_n3b97gsqi85HJx61j2h79MkEcehVRr9Uuzw6uwDItK9y4sKB82CKw/exec',
   storageKeys: Object.freeze({
     adminAccessKey: 'qr-entry-logger.admin-access-key',
   }),
