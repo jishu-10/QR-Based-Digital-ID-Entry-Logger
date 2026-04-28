fc8091bb0573a138cf3afd77b3752e79c07d1f3a
Snehanshu Das <156137321+jishu-10@users.noreply.github.com>
2026-04-29 02:00:36 +0530

    Update runtime-config.js

--- assets/js/runtime-config.js
+++ assets/js/runtime-config.js
@@ -1,6 +1,6 @@
 window.APP_RUNTIME_CONFIG = Object.freeze({
   appsScriptWebAppUrl:
-    'https://script.google.com/macros/s/AKfycby9B1BgbkLdnQ_g_n3b97gsqi85HJx61j2h79MkEcehVRr9Uuzw6uwDItK9y4sKB82CKw/exec',
+    'https://project-ya9xq.vercel.app/api/proxy',
   storageKeys: Object.freeze({
     adminAccessKey: 'qr-entry-logger.admin-access-key',
   }),
